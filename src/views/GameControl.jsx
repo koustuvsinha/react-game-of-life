ead7c5e09a3ccce802bfcb7ec25cc536b6919bb5
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-06-15 22:36:22 +0530

    Add clear button

--- src/views/GameControl.jsx
+++ src/views/GameControl.jsx
@@ -7,10 +7,15 @@ export default class GameControl extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   handleClick() {
     this.props.state.toggleGameState();
+  }
+
+  clear() {
+    this.props.state.prepareGameTable();
   }
 
   render() {
@@ -19,6 +24,10 @@ export default class GameControl extends Component {
         <button className="ui labeled icon button" onClick={this.handleClick}>
           <i className={`icon ${this.props.state.nextState.toLowerCase()}`}></i>
           {this.props.state.nextState}
+        </button>
+        <button className="ui labeled icon button" onClick={this.clear}>
+          <i className="icon clear"></i>
+          Clear
         </button>
       </div>
     );
